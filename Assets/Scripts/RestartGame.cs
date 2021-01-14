3cdd8f7e58991c9b6577a874523fe8f072eb0d19
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-01-14 11:46:04 -0500

    Completed tutorial

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Weight"))
+            StartCoroutine(ReloadDelay());
+    }
+
+    IEnumerator ReloadDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/RopeCutter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RopeCutter : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            // Since we don't want to use the ray to go anywhere, that's why we're using Vector2.zero. The ray is just being used as a point, since that's where the impact will be.
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Link"))
+                {
+                    Destroy(hit.collider.gameObject);
+                    //Destroy(hit.transform.parent.gameObject, 2f); //This would remove the entire rope after it is cut
+                }
+            }
+        }
+    }
+}   
